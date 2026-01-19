4736257605246c07bbbc94ee13d1cfa34d2b53b7
Eder Ribeiro <contato@ederribeiro.com.br>
2026-01-19 09:42:15 -0300

    Squeleton Stable Version 4.1.4

--- documentation/bricks/assets/js/state.js
+++ documentation/bricks/assets/js/state.js
@@ -11,7 +11,7 @@ function getInitialState() {
         <div class="f-grid f-items-center f-justify-center">
           <i class="fs-12 iccon-rocket-1 p-15-r animated delay-800 floatIn"></i>
           <h1 class="fs-16 m-25-tb fw-700 animated slideIn">Squeleton</h1>
-          <div class="fs-3 m-10-l p-5-lr border-rd-4 border-all animated magnetIn delay-800">v4.7.4</div>
+          <div class="fs-3 m-10-l p-5-lr border-rd-4 border-all animated magnetIn delay-800">v4.1.4</div>
         </div>
         <p class="fs-10 p-20-tb text-center animated delay-500 fadeInUp">Boilerplate leve e moderno que combina o melhor do CSS utilitário, grid responsivo, animações elegantes e bibliotecas JavaScript essenciais para criar interfaces rápidas e intuitivas.</p>
       </div>
